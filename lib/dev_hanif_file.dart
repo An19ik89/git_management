c7c624293c7211e4ff3f10e9ecc7e58f5541583c
anik <sajjad@tikweb.dk>
2024-01-17 21:14:49 +0600

    dev_hanif_file added

---
+++ lib/dev_hanif_file.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class DevHanif extends StatelessWidget {
+  const DevHanif({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Developer Hanif Branch'),
+          ],
+        ),
+      ),
+    );
+  }
+}
