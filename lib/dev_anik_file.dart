61d690f54c9732aa69184ae41ca61cef64a600ae
anik <sajjad@tikweb.dk>
2024-01-17 21:18:14 +0600

    dev_anik_file added

---
+++ lib/dev_anik_file.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class DevAnik extends StatelessWidget {
+  const DevAnik({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Developer Anik Branch'),
+          ],
+        ),
+      ),
+    );
+  }
+}
